e8cdbb9fa17053951b90ee2ea318f9b73714ad62
AdrKacz <adrien.kaczmarek@gmail.com>
2021-11-22 11:18:30 +0100

    Local MySQL Server

--- backend/database/model.sql
+++ backend/database/model.sql
@@ -23,40 +23,39 @@ CREATE TABLE `Consumer`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `number` INT NOT NULL UNIQUE,
     `surname` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    FOREIGN KEY(`departure_id`) REFERENCES `Departure`(`id`),
-    FOREIGN KEY(`consumer_id`) REFERENCES `Consumer`(`id`)
+    `name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE `Connection`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `departure_airport_id` INT NOT NULL,
-    `arrival_airport_id` INT NOT NULL,
+    `departure_airport_id` INT UNSIGNED NOT NULL,
+    `arrival_airport_id` INT UNSIGNED NOT NULL,
     FOREIGN KEY(`departure_airport_id`) REFERENCES `Airport`(`id`),
     FOREIGN KEY(`arrival_airport_id`) REFERENCES `Airport`(`id`)
 );
 
 CREATE TABLE `Pilot`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `employee_id` INT NOT NULL,
+    `employee_id` INT UNSIGNED NOT NULL,
     `license_number` INT NOT NULL UNIQUE,
     FOREIGN KEY(`employee_id`) REFERENCES `Employee`(`id`)
 );
 
 CREATE TABLE `CrewMember`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `employee_id` INT NOT NULL,
+    `employee_id` INT UNSIGNED NOT NULL,
     `role` VARCHAR(255) NOT NULL,
     FOREIGN KEY(`employee_id`) REFERENCES `Employee`(`id`)
 );
+
 
 CREATE TABLE `Flight`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `from` DATE NOT NULL,
     `to` DATE NOT NULL,
     `number` INT NOT NULL UNIQUE,
-    `connection_id` INT NOT NULL,
-    `airplane_id` INT NOT NULL,
+    `connection_id` INT UNSIGNED NOT NULL,
+    `airplane_id` INT UNSIGNED NOT NULL,
     `departure_day` VARCHAR(255) NOT NULL,
     `arrival_day` VARCHAR(255) NOT NULL,
     `departure_time` TIME NOT NULL,
@@ -67,14 +66,14 @@ CREATE TABLE `Flight`(
 
 CREATE TABLE `Departure`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `flight_id` INT NOT NULL,
+    `flight_id` INT UNSIGNED NOT NULL,
     `date` DATE NOT NULL,
-    `pilot_id` INT NOT NULL,
-    `optional_pilot_id` INT NOT NULL,
-    `first_crew_member_id` INT NOT NULL,
-    `second_crew_member_id` INT NOT NULL,
-    `number_of_empty_seat` INT NOT NULL,
-    `number_of_reserved_seat` INT NOT NULL,
+    `pilot_id` INT UNSIGNED NOT NULL,
+    `optional_pilot_id` INT UNSIGNED NOT NULL,
+    `first_crew_member_id` INT UNSIGNED NOT NULL,
+    `second_crew_member_id` INT UNSIGNED NOT NULL,
+    `number_of_empty_seat` INT UNSIGNED NOT NULL,
+    `number_of_reserved_seat` INT UNSIGNED NOT NULL,
     FOREIGN KEY(`flight_id`) REFERENCES `Flight`(`id`),
     FOREIGN KEY(`pilot_id`) REFERENCES `Pilot`(`id`),
     FOREIGN KEY(`optional_pilot_id`) REFERENCES `Pilot`(`id`),
@@ -87,6 +86,8 @@ CREATE TABLE `Ticket`(
     `number` INT NOT NULL UNIQUE,
     `date_time_of_issue` DATETIME NOT NULL,
     `price` INT NOT NULL,
-    `departure_id` INT NOT NULL,
-    `consumer_id` INT NOT NULL
+    `departure_id` INT UNSIGNED NOT NULL,
+    `consumer_id` INT UNSIGNED NOT NULL,
+    FOREIGN KEY(`departure_id`) REFERENCES `Departure`(`id`),
+    FOREIGN KEY(`consumer_id`) REFERENCES `Consumer`(`id`)
 );
---
+++ backend/mysql-server/commands/drop.js
@@ -0,0 +1,72 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+(async () => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'airlineapp',
+    });
+
+    const connect = () => (
+        new Promise((resolve, reject) => {
+            connection.connect((err, result) => {
+                if (err) {
+                    console.error('error connecting: ' + err.stack);
+                    reject(err);
+                }
+                console.log('connected as id ' + connection.threadId);
+                resolve(result)
+            })
+        }).catch((err) => {throw err})
+    );
+
+    const query = (q) => (
+        new Promise((resolve, reject) => {
+            connection.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(result)
+            })
+        }).catch((err) => {throw err})
+    );
+
+    await connect();
+
+    // Read Model Definition
+    let queries;
+    try {
+        const data = fs.readFileSync('../database/model.sql', 'utf-8');
+        queries = data.split(';');
+    } catch (error) {
+        console.error(error);
+        connection.end();
+        return;
+    }
+
+    // Loop over model synchronously to avoid dependency problem
+    for (let index = 0; index < queries.length - 1; index++) {
+        const q = queries[index].replace(/^\s+|\s+$/g, '');
+        // Extract table name
+        const raw_table = q.split(' ')[2];
+        const table = raw_table.substring(1, raw_table.length - 3);
+        try {
+            const result = await query(`DROP TABLE ${table}`);
+            console.log(`[${index}] Query`);
+            console.log(q);
+            console.log(`[${index}] Result`);
+            console.log(result);
+        } catch (error) {
+            console.error(`[${index}] Query`);
+            console.error(q);
+            console.error(`[${index}] Error`);
+            console.error(error);
+        } finally {
+            console.log(`[${index}] Job Done.`);
+        }  
+    }
+    connection.end();
+    return;
+})();
