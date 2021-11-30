b80bdb24a7c5f47546def0587ecfbd72f511108a
AdrKacz <adrien.kaczmarek@gmail.com>
2021-11-30 11:11:56 +0100

    Delete and Update panel

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -94,7 +94,9 @@ const databaseSchema = {
 }
 
 function Admin() {
-    const [create, setCreate] = useState({});
+    const [state, setState] = useState('create');
+
+    const [editedObject, setEditedObject] = useState({});
     const objects = useObjects([
         'airports',
         'employees',
@@ -107,26 +109,30 @@ function Admin() {
         'consumers'
     ]);
 
-    function handleChangeObject(object, field, value) {
-    if (create[object]) {
-        setCreate({
-            ...create,
-            [object]: {
-                ...create[object],
-                [field]: value,
-            },
-        });
-    } else {
-        setCreate({
-            ...create,
-            [object]: {
-                [field]: value,
-            },
-        });
-    }
+    function handleTabClick(name) {
+        setState(name);
     }
 
-    async function handleSubmitObject(object) {
+    function handleChangeObject(object, field, value) {
+        if (editedObject[object]) {
+            setEditedObject({
+                ...editedObject,
+                [object]: {
+                    ...editedObject[object],
+                    [field]: value,
+                },
+            });
+        } else {
+            setEditedObject({
+                ...editedObject,
+                [object]: {
+                    [field]: value,
+                },
+            });
+        }
+    }
+
+    async function handleSubmitCreateObject(object) {
         const responseJSON = await fetch('http://127.0.0.1:8080/create', {
             method: 'POST',
             headers: {
@@ -134,51 +140,168 @@ function Admin() {
             },
             body: JSON.stringify({
                 object: object,
-                data: create[object]
+                data: editedObject[object]
             }),
         }).then(response => response.json());
 
         console.log(responseJSON);
     }
 
-    const forms = Object.entries(databaseSchema).map(([key, value], i) => (
-    <form key={i} className='mb-5' onSubmit={(e) => {e.preventDefault(); handleSubmitObject(key)}}>
-        <h4 className='mb-3'>{capitalize(key)}</h4>
-        {
-            value.fields.map(([field, type], j) => {
-                if (type[0] === '$') {
-                    const [localObject, localFieldsString] = type.slice(1).split('$');
-                    const localFields = localFieldsString.split('.')
-                    console.log(objects, localObject, objects[localObject + 's'])
-                    return (
-                        <div key={`${i}$${j}`} className='form-floating mb-3'>
-                            <select
-                                value={create[key] ? create[key][field] : undefined}
-                                onChange={({target}) => (handleChangeObject(key, field, target.value))}
-                                className='form-select'
-                                id={`floatingselect$${key}$${field}`}
-                            >
-                                {(objects[localObject + 's'] || []).map((o, i) => (
-                                    <option key={`floatingselect$${key}$${field}$${i}`} value={o.id}>{localFields.map(f => o[f]).join(' ')}</option>
-                                ))}
-                            </select>
-                            <label htmlFor={`floatingselect$${key}$${field}`}>{capitalize(field)}</label>
-                        </div>
-                    );
-                } else {
-                    return (
-                        <div key={`${i}$${j}`} className='form-floating mb-3'>
-                            <input onChange={({target}) => (handleChangeObject(key, field, target.value))} type={type} className='form-control' id={`floating$${key}$${field}`} placeholder={field} required/>
-                            <label htmlFor={`floating$${key}$${field}`}>{capitalize(field)}</label>
-                        </div>
-                    );
-                }
-            })
-        }
-        <button className="w-100 btn btn-lg btn-primary" type="submit">Create</button>
-    </form>
-    ));
+    async function handleSubmitDeleteObject(object) {
+        const responseJSON = await fetch('http://127.0.0.1:8080/delete', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                object: object,
+                objectId: editedObject[object]
+            }),
+        }).then(response => response.json());
 
+        console.log(responseJSON);
+    }
+
+    let forms;
+    if (state === 'create') {
+        forms = Object.entries(databaseSchema).map(([key, value], i) => (
+            <form key={i} className='mb-5' onSubmit={(e) => {e.preventDefault(); handleSubmitCreateObject(key)}}>
+                <h4 className='mb-3'>{capitalize(key)}</h4>
+                {
+                    value.fields.map(([field, type], j) => {
+                        if (type[0] === '$') {
+                            const [localObject, localFieldsString] = type.slice(1).split('$');
+                            const localFields = localFieldsString.split('.')
+                            return (
+                                <div key={`${i}$${j}`} className='form-floating mb-3'>
+                                    <select
+                                        value={editedObject[key] ? editedObject[key][field] : undefined}
+                                        onChange={({target}) => (handleChangeObject(key, field, target.value))}
+                                        className='form-select'
+                                        id={`floatingselect$${key}$${field}`}
+                                    >
+                                        {(objects[localObject + 's'] || []).map((o, i) => (
+                                            <option key={`floatingselect$${key}$${field}$${i}`} value={o.id}>{localFields.map(f => o[f]).join(' ')}</option>
+                                        ))}
+                                    </select>
+                                    <label htmlFor={`floatingselect$${key}$${field}`}>{capitalize(field)}</label>
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div key={`${i}$${j}`} className='form-floating mb-3'>
+                                    <input
+                                        onChange={({target}) => (handleChangeObject(key, field, target.value))}
+                                        type={type}
+                                        className='form-control'
+                                        id={`floating$${key}$${field}`}
+                                        placeholder={field}
+                                        value={editedObject[key] ? editedObject[key][field] : ''}
+                                        required/>
+                                    <label htmlFor={`floating$${key}$${field}`}>{capitalize(field)}</label>
+                                </div>
+                            );
+                        }
+                    })
+                }
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Create</button>
+            </form>
+            ));
+    } else if (state === 'update') {
+        forms = Object.entries(databaseSchema).map(([key, value], i) => (
+            <form key={i} className='mb-5' onSubmit={(e) => {e.preventDefault(); handleSubmitCreateObject(key)}}>
+                <h4 className='mb-3'>{capitalize(key)}</h4>
+                <div key={`selectmain$${i}`} className='form-floating mb-3'>
+                    <select
+                            value={editedObject[key] ? editedObject[key].id : undefined}
+                            onChange={({target}) => (handleChangeObject(key, 'id', parseInt(target.value)))}
+                            className='form-select'
+                            id={`floatingselectmain${key}`}
+                        >
+                            <option>...</option>
+                            {(objects[key + 's'] || []).map((o, k) => (
+                                <option key={`floatingselectmain$${key}$${k}`} value={o.id}>{o.id}</option>
+                            ))}
+                    </select>
+                    <label htmlFor={`floatingselectmain$${key}`}>{capitalize(key)}</label>
+                </div>
+                {editedObject[key] && editedObject[key].id &&
+                    value.fields.map(([field, type], j) => {
+                        if (type[0] === '$') {
+                            const [localObject, localFieldsString] = type.slice(1).split('$');
+                            const localFields = localFieldsString.split('.')
+                            return (
+                                <div key={`${i}$${j}`} className='form-floating mb-3'>
+                                    <select
+                                        value={editedObject[key] ? editedObject[key][field] : undefined}
+                                        onChange={({target}) => (handleChangeObject(key, field, target.value))}
+                                        className='form-select'
+                                        id={`floatingselect$${key}$${field}`}
+                                    >
+                                        {(objects[localObject + 's'] || []).map((o, i) => (
+                                            <option key={`floatingselect$${key}$${field}$${i}`} value={o.id}>{localFields.map(f => o[f]).join(' ')}</option>
+                                        ))}
+                                    </select>
+                                    <label htmlFor={`floatingselect$${key}$${field}`}>{capitalize(field)}</label>
+                                </div>
+                            );
+                        } else {
+                            let value = '';
+                            console.log('---', key)
+                            if (editedObject[key]) {
+                                if (editedObject[key][field]) {
+                                    value = editedObject[key][field]
+                                } else {
+                                    const item = (objects[key + 's'] || []).find(el => el.id === editedObject[key].id);
+                                    if (item) {
+                                        value = item[field];
+                                    }
+                                }
+                            }
+                            return (
+                                <div key={`${i}$${j}`} className='form-floating mb-3'>
+                                    <input
+                                        onChange={({target}) => (handleChangeObject(key, field, target.value))}
+                                        type={type}
+                                        className='form-control'
+                                        id={`floating$${key}$${field}`}
+                                        placeholder={field}
+                                        value={value}
+                                        required/>
+                                    <label htmlFor={`floating$${key}$${field}`}>{capitalize(field)}</label>
+                                </div>
+                            );
+                        }
+                    })
+                }
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Update</button>
+            </form>
+            ));
+    } else if (state === 'delete') {
+        forms = Object.entries(databaseSchema).map(([key, value], i) => (
+            <form key={i} className='mb-5' onSubmit={(e) => {e.preventDefault(); handleSubmitDeleteObject(key)}}>
+                <h4 className='mb-3'>{capitalize(key)}</h4>
+                <div key={`selectmain$${i}`} className='form-floating mb-3'>
+                    <select
+                        value={editedObject[key] ? editedObject[key].id : undefined}
+                        onChange={({target}) => (handleChangeObject(key, 'id', parseInt(target.value)))}
+                        className='form-select'
+                        id={`floatingselectmain$${key}`}
+                    >
+                        <option>...</option>
+                        {(objects[key + 's'] || []).map((o, k) => (
+                            <option key={`floatingselectmain$${key}$${k}`} value={o.id}>{o.id}</option>
+                        ))}
+                    </select>
+                    <label htmlFor={`floatingselectmain$${key}`}>{capitalize(key)}</label>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Delete</button>
+            </form>
+            ));
+    }
+
+
+    const tabClassName = (name) => (`${(name === state) ? 'active ' : ''}nav-link btn btn-link`)
 
     return (
     <div className='container pb-5'>
@@ -187,6 +310,17 @@ function Admin() {
             <h2>You're the boss here!</h2>
             <p className='lead'>Just do anything you want, fire is in your hand.</p>
         </div>
+        <ul className='nav nav-tabs mb-3'>
+            <li className='nav-item'>
+                <button onClick={(e) => (handleTabClick('create'))} type='button' className={tabClassName('create')}>Create</button>
+            </li>
+            <li className='nav-item'>
+                <button onClick={(e) => (handleTabClick('update'))} type='button' className={tabClassName('update')}>Update</button>
+            </li>
+            <li className='nav-item'>
+                <button onClick={(e) => (handleTabClick('delete'))} type='button' className={tabClassName('delete')}>Delete</button>
+            </li>
+        </ul>
         {forms}
         </main>
     </div>
