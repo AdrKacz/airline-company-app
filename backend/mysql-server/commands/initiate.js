552fc8156f38d7138f298a9d55a3e6bab7c70f7f
AdrKacz <adrien.kaczmarek@gmail.com>
2021-12-02 16:24:06 +0100

    .env to commands

--- backend/mysql-server/commands/initiate.js
+++ backend/mysql-server/commands/initiate.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const mysql = require('mysql');
 const fs = require('fs');
 const {connect, query} = require('../helpers/mysql-helpers');
 
 (async () => {
     const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'airlineapp',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
 
     await connect(connection);
